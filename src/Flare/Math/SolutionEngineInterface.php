99c813366f1082e1b1085f5566788da527cafe4e
Johnathon Koster <johnathon.koster@paradoxone.com>
2013-08-16 00:51:06 -0500

    Allow execution engine
    A change to the interface to allow the solution interface to use an
    implementation of the MathExecutionEngine

--- src/Flare/Math/SolutionEngineInterface.php
+++ src/Flare/Math/SolutionEngineInterface.php
@@ -10,4 +10,17 @@ interface SolutionEngineInterface {
 	 */
 	public function solve($equation);
 
+	/**
+	 * Sets the ExecutionEngineInterface implementation.
+	 *
+	 * @param \Flare\Math\ExecutionEngineInterface
+	 */
+	public function setExecutionEngine(\Flare\Math\ExecutionEngineInterface $executionEngine);
+
+	/**
+	 * Gets the ExecutionEngineInterface implementation.
+	 *
+	 */
+	public function setExecutionEngine();
+
 }
